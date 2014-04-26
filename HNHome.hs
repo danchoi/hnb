998c219ef51868879e1f720f565624506438e5f3
Daniel Choi <dhchoi@gmail.com>
2014-04-25 20:10:32 -0400

    listA parsedItems2

--- HNHome.hs
+++ HNHome.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE Arrows, NoMonomorphismRestriction, OverloadedStrings, ScopedTypeVariables #-}
 module Main where
 import Text.XML.HXT.Core
+import qualified Data.List.Split as L
 
 
 data Item = Item {
@@ -11,7 +12,11 @@ data Item = Item {
   -- , points :: String
   } deriving (Show)
 
+data Item2 = Item2 {
+  } deriving (Show)
+
 items = (isElem >>> hasName "tr") `containing` tdRank
+items2 = (isElem >>> hasName "tr") `containing` tdSubtext
 
 
 -- gets next TR row after 1st one
@@ -22,31 +27,27 @@ nextSibling = addNav
     >>> isElem
 
 tdRank = (deep getChildren >>> hasName "td" >>> hasAttrValue "class" (== "title") )
+tdSubtext = (deep getChildren >>> hasName "td" >>> hasAttrValue "class" (== "subtext") )
 
 tdTitleNode = (getChildren >>> isElem >>> hasName "td") 
 
 
-parsedItems = proc x -> do
+parsedItems1 = proc x -> do
     r <- tdRank >>> getChildren >>> getText -< x
     t <- tdTitleNode /> isElem >>> hasName "a" >>> getChildren >>> getText -< x
     d <- tdTitleNode /> isElem >>> hasName "span" >>> getChildren >>> getText -< x
     h <- tdTitleNode /> isElem >>> hasName "a" >>> getAttrValue "href" -< x
     -- p <- nextSibling >>> (deep getText) -< x
-    returnA -< Item r t d h 
+    returnA -< (r, t, d, h)
 
+parsedItems2 = proc x -> do
+    a <- tdSubtext //> getText -< x
+    returnA -< a
 
 main = do 
   html <- getContents 
   let doc = readString [withParseHTML yes, withWarnings no] html
-  links <- runX $ doc //> items >>> parsedItems
-  
-  -- print links
-
-  links2 <- runX $ doc //> hasName "table" >>> addNav 
-    //> 
-    withoutNav items  
-    >>> listA followingSiblingAxis >>> arr head 
-    >>> remNav
-    
-
-  print links2
+  links <- runX $ doc //> items >>> parsedItems1
+  print links
+  links2 <- runX $ doc //> items2 >>> listA parsedItems2
+  mapM_ print links2
